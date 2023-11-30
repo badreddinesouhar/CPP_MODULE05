1858aac2029d9ce2c5e5147455231440688f6760
Badr eddine Souhar <bsouhar@c1r3p10.1337.ma>
2023-11-30 13:13:49 +0100

    cpp

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: bsouhar <bsouhar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 10:23:16 by bsouhar           #+#    #+#             */
-/*   Updated: 2023/11/30 11:12:06 by bsouhar          ###   ########.fr       */
+/*   Updated: 2023/11/30 13:00:39 by bsouhar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 #define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <exception>
 
 class Bureaucrat {
   private:
     const std::string _name;
     int _grade;
   public:
-    //orthodox canolical form
     Bureaucrat();
     ~Bureaucrat();
     Bureaucrat(int grade);
@@ -28,24 +28,19 @@ class Bureaucrat {
     Bureaucrat(const std::string name);
     Bureaucrat(const Bureaucrat& copy);
     Bureaucrat& operator=(const Bureaucrat& bureaucrat);
-    // getteres
     const std::string getName() const;
     int getGrade() const;
-    //increment and decrement functions
+    void setGrade(int grade);
     void gradeIncrement();
     void gradeDecrement();
-    //exceptions classes
     class GradeTooHighException : public std::exception {
-      const char* what() const throw() {
-        std::cout << "Grade are too high" << std::endl;
-      }
+      public:
+        const char* what() const throw();
     };
     class GradeTooLowException : public std::exception {
-      const char* what() const throw() {
-        std::cout << "Grade are too low" << std::endl;
-      }
+      public:
+        const char* what() const throw();
     };
-    
 };
 
 std::ostream& operator << (std::ostream& out, const Bureaucrat& bereaucrat);
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,27 @@
 /*   By: bsouhar <bsouhar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 13:03:38 by bsouhar           #+#    #+#             */
-/*   Updated: 2023/11/30 10:36:23 by bsouhar          ###   ########.fr       */
+/*   Updated: 2023/11/30 13:13:33 by bsouhar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main() {
-    Bureaucrat();
-    Bureaucrat("aba");
+    try {
+        Bureaucrat("badr", 15); 
+        Bureaucrat a;
+        // std::cout << a << std::endl;
+        // a.setGrade(1500);
+        // std::cout << a << std::endl;
+        // Bureaucrat(-1); 
+        Bureaucrat b("anass");
+        b.setGrade(150);
+        b.gradeIncrement();
+        b.gradeDecrement();
+        // b.gradeDecrement();
+    }
+    catch(std::exception & e) {
+        std::cout << e.what() << std::endl;
+    }
 }
